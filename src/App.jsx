71ecf2530f2a46f166d1d0b12ed36042d017de94
sudhanshulicensed <sudhanshu.licensed@gmail.com>
2024-10-13 19:06:08 +0530

    Feature : Use global CSS, setup fonts and reset css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import React from "react"
-import Header from "@/components/HeaderComponent"
-import Main from "@/components/MainComponent"
-import Footer from "@/components/FooterComponent"
+import React from "react";
+import Main from "@/components/MainComponent";
+import Footer from "@/components/FooterComponent";
+import './App.css'
 
-export default function App () {
+export default function App() {
   return (
-    <>
-      <Header />
-      <Main /> 
+    <div className="app">
+      <section id="section1" className="section">
+        Section 2
+      </section>
+      <section id="section2" className="section">
+        Section 2
+      </section>
+      <Main />
       <Footer />
-    </>
-  )
+    </div>
+  );
 }
