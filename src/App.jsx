1cad22bb3cfc909f6fd45bda21dd09bf534d39a9
sudhanshulicensed <sudhanshu.licensed@gmail.com>
2024-10-14 14:22:07 +0530

    Chore : Update Directory to utilize MainComponent.

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,6 @@ import React from "react";
 import Header from "@/components/HeaderComponent";
 import Main from "@/components/MainComponent";
 import Footer from "@/components/FooterComponent";
-import Banner from "@/components/Banner";
-import Event from "@/components/Events";
-import EventExperience from "@/components/Experience";
-import ExperienceDemo from "@/components/ExperienceDemo";
-import Highlights from "@/components/Highlights";
-import Schedule from "@/components/Schedule";
-import Upcoming from "@/components/Upcoming";
-import Destination from "@/components/Destination";
-import NewsLetter from "@/components/NewsLetter";
 
 import "./App.css";
 
@@ -18,33 +9,6 @@ export default function App() {
   return (
     <div className="app">
       <Header />
-      <section id="section1" className="section">
-        <Banner />
-      </section>
-      <section id="section2" className="section">
-        <Event />
-      </section>
-      <section id="section3" className="section">
-        <EventExperience />
-      </section>
-      <section id="section4" className="section">
-        <ExperienceDemo />
-      </section>
-      <section id="section5" className="section">
-        <Highlights />
-      </section>
-      <section id="section6" className="section">
-        <Schedule />
-      </section>
-      <section id="section7" className="section">
-        <Upcoming />
-      </section>
-      <section id="section8" className="section">
-        <Destination />
-      </section>
-      <section id="section9">
-        <NewsLetter />
-      </section>
       <Main />
       <Footer />
     </div>
--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,8 +1,44 @@
 import React from "react";
+import Banner from "@/components/Banner";
+import Event from "@/components/Events";
+import EventExperience from "@/components/Experience";
+import ExperienceDemo from "@/components/ExperienceDemo";
+import Highlights from "@/components/Highlights";
+import Schedule from "@/components/Schedule";
+import Upcoming from "@/components/Upcoming";
+import Destination from "@/components/Destination";
+import NewsLetter from "@/components/NewsLetter";
+
 export default function MainComponent() {
   return (
     <main>
-      <h2>Your Main Content Here</h2>
+      <section id="section1" className="section">
+        <Banner />
+      </section>
+      <section id="section2" className="section">
+        <Event />
+      </section>
+      <section id="section3" className="section">
+        <EventExperience />
+      </section>
+      <section id="section4" className="section">
+        <ExperienceDemo />
+      </section>
+      <section id="section5" className="section">
+        <Highlights />
+      </section>
+      <section id="section6" className="section">
+        <Schedule />
+      </section>
+      <section id="section7" className="section">
+        <Upcoming />
+      </section>
+      <section id="section8" className="section">
+        <Destination />
+      </section>
+      <section id="section9">
+        <NewsLetter />
+      </section>
     </main>
   );
 }
