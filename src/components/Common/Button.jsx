ffea64911ddbe10249a5b06c6edbb4c6b484dfec
sudhanshulicensed <sudhanshu.licensed@gmail.com>
2024-10-14 13:05:13 +0530

    Chore : Make Button Class accept children prop.

--- src/components/Common/Button.jsx
+++ src/components/Common/Button.jsx
@@ -1,3 +1,6 @@
-export default function Button({title}) {
-    return <a href="#destination" className="button font-w-light font-s-medium">{title}</a>
+export default function Button({title, children}) {
+    return <a href="#destination" className="button font-w-light font-s-medium">
+        {children && <span className="button__icon">{children}</span>}
+        <span className="button__text">{title}</span>
+    </a>
 }
