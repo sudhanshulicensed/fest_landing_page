db250fa89f07c5e3cf04dbd73628d5172473d2d6
sudhanshulicensed <sudhanshu.licensed@gmail.com>
2024-10-14 18:11:00 +0530

    Chore : Use Toggle for Responsive Header.

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,44 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import ToggleNavigation from "@/assets/images/ToggleNavigation.svg";
 import Button from "@/components/Common/Button";
 import "./Header.scss";
 
 export default function HeaderComponent() {
   const navigationItem = [
-    "Gallery",
-    "About Us",
-    "How It Works",
-    "Contact",
+    { text: "Gallery", value: "gallery" },
+    { text: "About Us", value: "about_us" },
+    { text: "How It Works", value: "how_it_works" },
+    { text: "Contact", value: "contact" },
   ];
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   return (
     <header className="header">
       <div className="header__logo">
         <img src="/jolFest.svg" alt="Logo" className="header__logo-img" />
       </div>
-      <nav className="navigation">
+      <button
+        className="mobile-menu-toggle"
+        onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+      >
+        <img src={ToggleNavigation} alt="Toggle Navigation" />
+      </button>
+      <nav className={`${isMobileMenuOpen && "navigation--open"} navigation`}>
         <ul className="navigation__list">
           {navigationItem.map((item) => {
             return (
-             <li key={item} className="navigation__item">
-               <a
-                href={item}
-                className="navigation__link font-w-light font-s-medium"
-              >
-                {item}
-              </a>
-             </li>
+              <li key={item.value} className="navigation__item">
+                <a
+                  href={`#${item.value}`}
+                  className="navigation__link font-w-light font-s-medium"
+                >
+                  {item.text}
+                </a>
+              </li>
             );
           })}
         </ul>
-        <ul className="navigation__list">
+        {isMobileMenuOpen && (
+          <ul className="navigation__list">
+            <li className="navigation__item">
+              <a
+                href="#login"
+                className="navigation__link font-w-light font-s-medium"
+              >
+                Login
+              </a>
+            </li>
+            <li className="navigation__item">
+              <Button title={"Destination"} />
+            </li>
+          </ul>
+        )}
+      </nav>
+      <ul className="extras navigation__list">
         <li className="navigation__item">
-          <a href="#login" className="navigation__link font-w-light font-s-medium">Login
+          <a
+            href="#login"
+            className="navigation__link font-w-light font-s-medium"
+          >
+            Login
           </a>
         </li>
         <li className="navigation__item">
-          <Button title={'Destination'}/>
+          <Button title={"Destination"} />
         </li>
-        </ul>
-      </nav>
+      </ul>
     </header>
   );
 }
