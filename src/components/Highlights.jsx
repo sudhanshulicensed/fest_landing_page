80ab6b81af8c52dbf677b41ae51a2c67d03439c7
sudhanshulicensed <sudhanshu.licensed@gmail.com>
2024-10-14 16:24:05 +0530

    Chore : Add Pointer Class, Add scroll feature to Highlights.

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,19 +1,65 @@
+import React, { useState } from "react";
 import GeneralTicket from "@/assets/images/Section5_Button.svg";
 import GeneralTicketIcon from "@/assets/images/Section5_Button2_Prefix.svg";
 import Scroll from "@/assets/images/Section5_Scroll.svg";
 import GeneralImage1 from "@/assets/images/Section5_G1.jpg";
 import GeneralImage2 from "@/assets/images/Section5_G2.jpg";
 import GeneralImage3 from "@/assets/images/Section5_G3.jpg";
-// import GeneralImage4 from "@/assets/images/Section5_G4.jpg";
-// import GeneralImage5 from "@/assets/images/Section5_G5.jpg";
-// import VIPImage1 from "@/assets/images/Section5_VIP1.jpg";
-// import VIPImage2 from "@/assets/images/Section5_VIP2.jpg";
-// import VIPImage3 from "@/assets/images/Section5_VIP3.jpg";
-// import VIPImage4 from "@/assets/images/Section5_VIP4.jpg";
-// import VIPImage5 from "@/assets/images/Section5_VIP5.jpg";
+import GeneralImage4 from "@/assets/images/Section5_G4.jpg";
+import GeneralImage5 from "@/assets/images/Section5_G5.jpg";
+import VIPImage1 from "@/assets/images/Section5_VIP1.jpg";
+import VIPImage2 from "@/assets/images/Section5_VIP2.jpg";
+import VIPImage3 from "@/assets/images/Section5_VIP3.jpg";
+import VIPImage4 from "@/assets/images/Section5_VIP4.jpg";
+import VIPImage5 from "@/assets/images/Section5_VIP5.jpg";
 import "./Highlights.css";
 
 export default function Highlights() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isVIP, setIsVIP] = useState(false);
+  const imagesToShow = 3;
+  
+  const images = [
+    GeneralImage1,
+    GeneralImage2,
+    GeneralImage3,
+    GeneralImage4,
+    GeneralImage5,
+  ];
+  const vipImages = [
+    VIPImage1,
+    VIPImage2,
+    VIPImage3,
+    VIPImage4,
+    VIPImage5,
+  ];
+  const handleLeftClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex <= 0 ? (isVIP ? vipImages.length - imagesToShow : images.length - imagesToShow) : prevIndex - imagesToShow
+    );
+  };
+  const handleRightClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex >= (isVIP ? vipImages.length - imagesToShow : images.length - imagesToShow) ? 0 : prevIndex + imagesToShow
+    );
+  };
+
+  const getVisibleImages = () => {
+    const currentImages = isVIP ? vipImages : images;
+    return currentImages.slice(currentIndex, currentIndex + imagesToShow);
+  };
+
+  const showLeftArrow = currentIndex > 0;
+  const showRightArrow = currentIndex < (isVIP ? vipImages.length - imagesToShow : images.length - imagesToShow);
+  
+  const handleVIPClick = () => {
+    setIsVIP(true);
+    setCurrentIndex(0);
+  };
+  const handleGeneralClick = () => {
+    setIsVIP(false);
+    setCurrentIndex(0);
+  };
   return (
     <section className="highlights">
       <p className="highlights__label">FESTIVAL MAGIC</p>
@@ -21,7 +67,7 @@ export default function Highlights() {
         Dive into the Highlights
       </p>
       <div className="highlights__button">
-        <div className="general-tour">
+        <div onClick={handleGeneralClick} className="general-tour pointer">
           <img
             src={GeneralTicket}
             alt="General Tour Ticket"
@@ -31,38 +77,35 @@ export default function Highlights() {
             General Tour Ticket
           </p>
         </div>
-        <div className="vip-tour">
+        <div onClick={handleVIPClick} className="vip-tour pointer">
           <img className="vip-tour__button-prefix" src={GeneralTicketIcon} alt="" />
           <p className="vip-tour__button-text font-w-medium">VIP Tour Ticket</p>
         </div>
       </div>
       <hr className="highlights__divider" />
       <div className="highlights__carousel">
-        <div className="highlights__carousel-images">
-          <img src={GeneralImage1} alt="" className="highlights__image" />
-          <img src={GeneralImage2} alt="" className="highlights__image" />
-          <img src={GeneralImage3} alt="" className="highlights__image" />
-          {/* <img src={GeneralImage4} alt="" className="highlights__image" />
-          <img src={GeneralImage5} alt="" className="highlights__image" /> */}
-        </div>
-        {/* <div className="highlights__carousel-images">
-          <img src={VIPImage1} alt="" className="highlights__image" />
-          <img src={VIPImage2} alt="" className="highlights__image" />
-          <img src={VIPImage3} alt="" className="highlights__image" />
-          <img src={VIPImage4} alt="" className="highlights__image" />
-          <img src={VIPImage5} alt="" className="highlights__image" />
-        </div> */}
+      <div className="highlights__carousel-images">
+        {getVisibleImages().map((image, index) => (
+          <img key={index} src={image} alt={`Highlight ${index}`} className="highlights__image" />
+        ))}
+      </div>
+      {showLeftArrow && (
         <img
-          className="highlights__carousel-action-left"
+          className="highlights__carousel-action-left pointer"
           src={Scroll}
           alt="Scroll Left"
+          onClick={handleLeftClick}
         />
+      )}
+      {showRightArrow && (
         <img
-          className="highlights__carousel-action-right"
+          className="highlights__carousel-action-right pointer"
           src={Scroll}
           alt="Scroll Right"
+          onClick={handleRightClick}
         />
-      </div>
+      )}
+    </div>
     </section>
   );
 }
