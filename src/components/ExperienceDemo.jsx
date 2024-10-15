4922548164612c548d52bcbb4a1bdc57d88641c6
sudhanshulicensed <sudhanshu.licensed@gmail.com>
2024-10-15 09:03:03 +0530

    Chore : Use 100% Height to accomodate image without overflowing..

--- src/components/ExperienceDemo.jsx
+++ src/components/ExperienceDemo.jsx
@@ -4,14 +4,14 @@ import './ExperienceDemo.css'
 
 export default function ExperienceDemo() {
     return (
-      <section className="experience-demo">
+      <article className="experience-demo">
         <p className="experience-demo__label font-w-regular font-s-medium">WHERE MUSIC AND MOUNTAIN SPIRIT COLLIDE</p>
         <p className="experience-demo__title font-w-medium text-center">Experience the Electric Mountain Festival</p>
         <div className="experience-demo__video">
           <img src={VideoThumbnail} alt="Video Thumbnail for Experience" className="experience-demo__video-image" />
           <img src={PlayPause} alt="Play Icon" className="experience-demo__video-icon" />
         </div>
-      </section>
+      </article>
     );
   }
   
